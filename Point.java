8da04778a2a48ad64071f740a392d1cb847eb359
NateshKukreja <natesh.kukreja@gmail.com>
2016-03-13 19:11:30 -0400

    helper class that stores a 2 dimentional element on a grid

--- Point.java
+++ Point.java
@@ -16,7 +16,7 @@ public class Point {
     }
 
     public void reset(int x, int y){
-        // REPLACE THE BODY OF THIS METHOD WITH YOUR OWN IMPLEMENTATION
+
         this.x = 0;
         this.y = 0;
     }
