df74b916fc043523bc875f7efb8e1cf2a4d6c87e
NateshKukreja <natesh.kukreja@gmail.com>
2016-03-13 19:26:25 -0400

    Starts the game

---
+++ CircleTheDot.java
@@ -0,0 +1,19 @@
+
+public class CircleTheDot {
+
+     public static void main(String[] args) {
+        int size = 9;
+        if (args.length == 1) {
+            try{
+                size = Integer.parseInt(args[0]);
+                if(size<4){
+                    System.out.println("Invalide argument, using default...");
+                    size = 9;
+                }
+            } catch(NumberFormatException e){
+                System.out.println("Invalide argument, using default...");
+            }
+        }
+        GameController game = new GameController(size);
+    }
+}
