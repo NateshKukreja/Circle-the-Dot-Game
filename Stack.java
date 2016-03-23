0e11ab2798bd00ce9bc0a2a0f01984540c280b0f
Natesh Kukreja <natesh.kukreja@gmail.com>
2016-03-22 23:21:38 -0400

    Added files via upload

---
+++ Stack.java
@@ -0,0 +1,13 @@
+
+public interface Stack<E> {
+
+
+    public abstract boolean isEmpty(); //checks if its empty
+
+    public abstract E peek(); //returns top element without removing it
+
+    public abstract E pop(); //returns top element and removes it
+
+    public abstract void push( E element ); //adds new element to stack
+
+}
