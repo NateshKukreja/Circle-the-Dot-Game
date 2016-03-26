91623208dcee24a6d5e1b8f411369fde36c2a09e
Natesh Kukreja <natesh.kukreja@gmail.com>
2016-03-26 11:05:01 -0400

    Update Stack.java

--- Stack.java
+++ Stack.java
@@ -1,12 +1,14 @@
+import java.util.EmptyStackException;
 
 public interface Stack<E> {
 
 
     public abstract boolean isEmpty(); //checks if its empty
 
-    public abstract E peek(); //returns top element without removing it
+    public abstract E peek() throws EmptyStackException; //returns top element without removing it
 
-    public abstract E pop(); //returns top element and removes it
+    public abstract E pop() throws EmptyStackException; //returns top element and removes it
 
-    public abstract void push( E element ); //adds new element to stack
+    public abstract void push( E element ) throws NullPointerException;//adds new element to stack
+
 }
